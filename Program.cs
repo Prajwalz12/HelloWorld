dc4f13407d8aa887dcfacab439b9929cb14b6e28
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-05 18:35:14 +0530

    adding vstudio files

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        /// <summary>
+        /// this is xml comment
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
